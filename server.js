ea791669dea2328329a31f6950fa3140f2cd671f
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-03 22:03:06 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const port = 4111;
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('this is the API');
+});
+
+app.get('/test', (req, res) => {
+	res.send('test works');
+});
+
+app.listen(port, () =>
+	console.log(`listening on port http://localhost:${port}`)
+);
